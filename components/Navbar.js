b26ae497b25123cae85f11010580da86afb1d1c9
wahid022 <rahmanwahid022@gmail.com>
2024-12-27 16:22:22 +0530

    Added the Navbbar to Each Page

---
+++ components/Navbar.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import React from "react";
+import styles from "../src/styles/Home.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.mainnav}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -16,22 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <nav className={styles.mainnav}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+      
       <main className={styles.main}>
         <h1 className={styles.title}>Hunting Coder</h1>
 
